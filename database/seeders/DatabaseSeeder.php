ee1852702860a9752ef88d980d4bae30eee94f28
Boyet Notebook <boyet007@gmail.com>
2024-06-10 13:55:38 +0700

    backend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Counter;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Counter::factory(1)->create();
+        // \App\Models\Invoice::factory(5)->create();
+        \App\Models\InvoiceItem::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
