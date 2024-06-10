210ad19e03fcf0c263797f87620aeb939b808cae
Boyet Notebook <boyet007@gmail.com>
2024-06-10 15:06:07 +0700

    frontend

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoice;
 
 class InvoiceController extends Controller
 {
-    //
+    public function getAllInvoices() {
+        $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+        return response()->json([
+            'invoices' => $invoices
+        ], 200);
+    }
 }
---
+++ resources/js/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import InvoiceIndex from "../components/invoices/InvoiceIndex.vue";
+import NotFound from "../components/NotFound.vue";
+
+const routes = [
+    { path: '/', component: InvoiceIndex },
+    { path: '/:pathMatch(.*)*', component: NotFound },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
